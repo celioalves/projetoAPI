135ba2cf001c77004a55435a8bb24ca80afb809a
celioalves <craftsman7@gmail.com>
2023-03-31 13:59:38 -0300

    Criada a classe Matricula, com suas propriedades

--- escola/models.py
+++ escola/models.py
@@ -9,7 +9,7 @@ class Aluno(models.Model):
     def __str__(self):
         return self.nome
 
-class Curso(models.Model):
+class CCurso(models.Model):
     
     NIVEL = (
         ('B', 'Básico'),
@@ -23,3 +23,16 @@ class Curso(models.Model):
 
     def __str__(self):
         return self.descricao
+    
+
+class Matricula(models.Model):
+
+        PERIODO = (
+        ('M', 'Matutino'),
+        ('V', 'Vespertino'),
+        ('N', 'Noturno')
+    )
+        aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+        curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+        periodo = models.CharField(max_length=1, choices=PERIODO, blank=False, null=False, default='M')
+
